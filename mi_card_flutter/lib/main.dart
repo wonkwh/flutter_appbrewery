69c6d28fb505d561b3eb8da299106ff3e77cbaca
kwanghyun.won <wonkwh@gmail.com>
2019-05-15 23:21:20 +0900

    Card & ListTile Widget

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.teal,
           body: SafeArea(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 CircleAvatar(
                     radius: 50,
@@ -33,46 +34,36 @@ class MyApp extends StatelessWidget {
                       fontSize: 20.0,
                       fontWeight: FontWeight.bold),
                 ),
-                Container(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                        size: 20,
-                      ),
-                      SizedBox(width: 10),
-                      Text('+011 2333 9830',
-                          style: TextStyle(
-                              fontFamily: 'Source Sans Pro',
-                              fontSize: 20,
-                              color: Colors.teal.shade900))
-                    ],
+                SizedBox(
+                  height: 20, width: 150,
+                  child: Divider(
+                    color: Colors.teal.shade100
+                    ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal
+                    ),
+                    title: Text(
+                      '+10 10 2398 2389',
+                      style: TextStyle(color: Colors.teal.shade900),
+                    ),
                   ),
                 ),
-                Container(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                    child: ListTile(
+                      leading: Icon(
                         Icons.email,
                         color: Colors.teal,
-                        size: 20,
                       ),
-                      SizedBox(width: 10),
-                      Text('wonkwh@gmail.com',
+                      title: Text('wonkwh@gmail.comm',
                           style: TextStyle(
-                              fontFamily: 'Source Sans Pro',
-                              fontSize: 20,
-                              color: Colors.teal.shade900))
-                    ],
-                  ),
-                ),
+                              color: Colors.teal.shade900)),
+                    )),
               ],
             ),
           )),
