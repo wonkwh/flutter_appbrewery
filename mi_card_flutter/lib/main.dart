82b708561492b9fb088a87a883ef5b60e1d3cd99
kwanghyun.won <wonkwh@gmail.com>
2019-05-15 22:50:03 +0900

    Adding Material Icons

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -25,15 +25,54 @@ class MyApp extends StatelessWidget {
                       fontSize: 30.0,
                       fontWeight: FontWeight.bold),
                 ),
-                                Text(
+                Text(
                   "Developer",
                   style: TextStyle(
                       fontFamily: 'Source Sans Pro',
                       color: Colors.white,
                       fontSize: 20.0,
                       fontWeight: FontWeight.bold),
-                )
-
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                        size: 20,
+                      ),
+                      SizedBox(width: 10),
+                      Text('+011 2333 9830',
+                          style: TextStyle(
+                              fontFamily: 'Source Sans Pro',
+                              fontSize: 20,
+                              color: Colors.teal.shade900))
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                        size: 20,
+                      ),
+                      SizedBox(width: 10),
+                      Text('wonkwh@gmail.com',
+                          style: TextStyle(
+                              fontFamily: 'Source Sans Pro',
+                              fontSize: 20,
+                              color: Colors.teal.shade900))
+                    ],
+                  ),
+                ),
               ],
             ),
           )),
