baeeabbffb37416566edc6e5629835db19b34a5d
kwanghyun.won <wonkwh@gmail.com>
2022-05-30 23:20:11 +0900

    enable hot reload

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
-    ),
-  ));
+  runApp(const CardApp());
+}
+
+class CardApp extends StatelessWidget {
+  const CardApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+    );
+  }
 }
