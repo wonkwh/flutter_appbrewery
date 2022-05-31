6d21e4f48d4ef9f93d36af0f95bb9864877ecd68
kwanghyun.won <wonkwh@gmail.com>
2022-05-31 14:35:18 +0900

    add custom font

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const CardApp());
@@ -10,10 +11,43 @@ class CardApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
+      home: getHome(),
+    );
+  }
+
+  Widget getHome() {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('images/profile.jpeg'),
+          ),
+          Text(
+            "Kwanghyun.Won",
+            style: GoogleFonts.pacifico(
+              textStyle: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+          ),
+          Text(
+            "IOS DEVELOPER",
+            style: GoogleFonts.sourceSansPro(
+              textStyle: TextStyle(
+                color: Colors.teal.shade100,
+                fontSize: 20,
+                letterSpacing: 2.5,
+              ),
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
