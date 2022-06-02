f8f673acc0aeda483e4274798772e296574e6760
kwanghyun.won <wonkwh@gmail.com>
2022-06-02 14:16:46 +0900

    add icon widget

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -19,35 +19,94 @@ class CardApp extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.teal,
       body: SafeArea(
+        child: Center(
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const CircleAvatar(
-            radius: 50,
-            backgroundImage: AssetImage('images/profile.jpeg'),
-          ),
-          Text(
-            "Kwanghyun.Won",
-            style: GoogleFonts.pacifico(
-              textStyle: const TextStyle(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/profile.jpeg'),
+              ),
+              Text(
+                "Kwanghyun.Won",
+                style: GoogleFonts.pacifico(
+                  textStyle: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              Text(
+                "IOS DEVELOPER",
+                style: GoogleFonts.sourceSansPro(
+                  textStyle: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                  ),
+                ),
+              ),
+              Container(
                 color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      size: 30,
+                      color: Colors.teal.shade500,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "+44 323 3929 392",
+                      style: GoogleFonts.sourceCodePro(
+                          textStyle: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                      )),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ),
-          Text(
-            "IOS DEVELOPER",
-            style: GoogleFonts.sourceSansPro(
-              textStyle: TextStyle(
-                color: Colors.teal.shade100,
-                fontSize: 20,
-                letterSpacing: 2.5,
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      size: 30,
+                      color: Colors.teal.shade500,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "wonkwh@gmail.com",
+                      style: GoogleFonts.sourceCodePro(
+                          textStyle: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                      )),
+                    ),
+                  ],
+                ),
               ),
-            ),
+            ],
           ),
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
