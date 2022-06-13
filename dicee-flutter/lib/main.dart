7d2943c9db6b1880182caffd4a2737da6e504205
kwanghyun.won <wonkwh@gmail.com>
2022-06-13 16:17:59 +0900

    change state widget

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +10,7 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: DiceStatePage(),
       ),
     ),
   );
@@ -18,6 +19,75 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
+              onPressed: () {
+                print("press");
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image(
+                image: AssetImage('images/dice2.png'),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DiceStatePage extends StatefulWidget {
+  DiceStatePage({Key? key}) : super(key: key);
+
+  @override
+  State<DiceStatePage> createState() => _DiceStatePageState();
+}
+
+class _DiceStatePageState extends State<DiceStatePage> {
+  var leftDiceNumber = 2;
+  var rightDiceNumber = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image(
+                image: AssetImage('images/dice$leftDiceNumber.png'),
+              ),
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6);
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image(
+                image: AssetImage('images/dice$rightDiceNumber.png'),
+              ),
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6);
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
